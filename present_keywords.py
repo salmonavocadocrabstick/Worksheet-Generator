f6017607aab9812214961a78bb0fb0412cf0dff4
salmonavocadocrabstick <iLoveSalmon!1>
2019-01-02 18:03:37 +0800

    Replacing present tense file

---
+++ present_keywords.py
@@ -0,0 +1,14 @@
+from random import choice
+
+def present_tense_keyword():
+	keywords = [
+
+		"every day",
+		"every year",
+		"often",
+		"every Friday"
+		"every Monday"
+
+	]
+
+	return choice(keywords)
