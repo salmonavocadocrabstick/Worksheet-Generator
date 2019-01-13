68df65365ede5a611ff82eb51887f1a35f2b1c25
salmonavocadocrabstick <iLoveSalmon!1>
2019-01-13 21:36:09 +0800

    Decorator it is.
    Decorator seems to make much more sense for now. Adjectives can be split into many categories, and to be able to add them by choice (without having to make all of them) is a useful idea.

--- object_builder.py
+++ object_builder.py
@@ -2,80 +2,65 @@
 # 
 # Stephanie Leung (2019)
 from random import choice
+import nouns_gen as nouns_gen
+import adjective_gen as adj_gen
 
 
 class AbstractObject:
+	def get_type(self):
+		if self.item_type:
+			return self.item_type
 
-	@classmethod
-	def get_noun(self):
-		pass
+class Noun(AbstractObject):
+	def __init__(self, item_type):
+		self.item_type = item_type
+		self.noun = nouns_gen.noun_generate(self.item_type) 
 
-	@classmethod
+	# def get_noun(self):
+	# 	return 
+
+class AdjectiveDecorator(AbstractObject):
+	def __init__(self, decorated_obj, item_type):
+		self.decorated_obj = decorated_obj
+		self.item_type = item_type
+
 	def get_adj(self):
 		pass
 
 
-class Objects(AbstractObject):
-	def __init__(self, item_type):
-	# self.adj = adj
-	# self.noun = noun
-	self.item_type = item_type
-
-	def get_type(self):
-		return self.type
-
-
-class AbstractObjectDecorator(AbstractObject):
+class Quantity(AdjectiveDecorator):
 	def __init__(self, decorated_obj):
-		self.decorated_obj = decorated_obj
+		AdjectiveDecorator.__init__(self, decorated_obj)
 
-
-class Noun(AbstractObjectDecorator):
-	def __init__(self, decorated_obj):
-		AbstractObjectDecorator.__init__(self, decorated_obj)
-
-	def get_noun(self):
-		return noun_item  #!!!DEFINE THIS
-
-
-class Adjective(AbstractObjectDecorator):
-	def __init__(self, decorated_obj):
-		AbstractObjectDecorator.__init__(self, decorated_obj)
+	def get_adj(self):
 		return adjective_item #!!!DEFINE THIS
 
 
+class Quality(AdjectiveDecorator):
+	def __init__(self, decorated_obj):
+		AdjectiveDecorator.__init__(self, decorated_obj)
 
-# class ObjectBuilder:
-# 	@classmethod
-# 	def set_noun(self):
-# 		pass
-
-# 	@classmethod
-# 	def set_adj(self):
-# 		pass
+	def get_adj(self):
+		return adjective_item #!!!DEFINE THIS
 
 
-# class NounBuilder(ObjectBuilder):
-# 	def __init__(self, item_type):
-# 		self.noun_obj = Objects(item_type)
+class Size(AdjectiveDecorator):
+	def __init__(self, decorated_obj):
+		AdjectiveDecorator.__init__(self, decorated_obj)
 
-# 	def set_noun(self):
-# 		self.noun_object.noun = choice()
+	def get_adj(self):
+		return adjective_item #!!!DEFINE THIS
 
+class Color(AdjectiveDecorator):
+	def __init__(self, decorated_obj, item_type):
+		AdjectiveDecorator.__init__(self, decorated_obj, item_type)
 
-# class AdjectiveBuilder(ObjectBuilder):
-# 	def __init__(self, item_type):
-# 		self.adj_obj = Objects(item_type)
-
-# 	def set_adj(self):
-# 		self.adj_obj = Objects(item_type)
-
-# class NounAndAdjBuilder(ObjectBuilder):
-# 	def __init__(self):
-# 		self.noun_and_adj_object = Objects()
-# 	def set_noun(self):
-# 		pass
-# 	def set_adj(self):
-# 		pass
+	def get_adj(self):
+		return adj_gen.color_adj_generator(self.item_type)
 
 
+test1 = Noun("food")
+print(test1.noun)
+
+adj1 = Color(test1, test1.get_type())
+print(adj1.get_adj() + test1.noun)
